c172545ae03e965c44de85039f8c0f707ed61dda
Rajeevek <42300799+Rajeevek@users.noreply.github.com>
2021-09-26 22:05:07 +0530

    Update: MountNewYorkTaxiADLSData.py RajeevLoadDataToDataLake.scala RajeevMergeGreenAndYellowData.sql notebooks

---
+++ RajeevMergeGreenAndYellowData.sql
@@ -0,0 +1,53 @@
+-- Databricks notebook source
+--Merge data
+SELECT 'Green' AS TaxiType
+      , PickupTime
+      , DropTime
+      , PickupLocationId
+      , DropLocationId      
+      , TripTimeInMinutes
+      , TripType
+FROM global_temp.FactGreenTaxiTripData
+
+UNION
+
+SELECT 'Yellow' AS TaxiType
+      , PickupTime
+      , DropTime
+      , PickupLocationId
+      , DropLocationId
+      , TripTimeInMinutes
+      , TripType
+FROM global_temp.FactYellowTaxiTripData
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %scala
+-- MAGIC // Extract Taxi Zones data
+-- MAGIC var taxiZonesDF = spark
+-- MAGIC                     .read
+-- MAGIC                     .option("header", "true")
+-- MAGIC                     .option("inferSchema", "true")
+-- MAGIC                     .csv("/mnt/csvFiles/TaxiZones.csv")
+-- MAGIC 
+-- MAGIC taxiZonesDF.createOrReplaceTempView("TaxiZones") 
+
+-- COMMAND ----------
+
+
+SELECT Borough, TaxiType, COUNT(*) AS TotalSharedTrips
+FROM TaxiZones
+LEFT JOIN
+(
+
+    SELECT 'Green' AS TaxiType, PickupLocationId FROM global_temp.FactGreenTaxiTripData WHERE TripType = 'SharedTrip'    
+    UNION ALL
+    SELECT 'Yellow' AS TaxiType, PickupLocationId FROM global_temp.FactYellowTaxiTripData WHERE TripType = 'SharedTrip'    
+    
+) AllTaxis
+ON AllTaxis.PickupLocationId = TaxiZones.LocationID
+
+GROUP BY Borough, TaxiType
+ORDER BY Borough, TaxiType
